b3154da7dbd88c9767e02cf1612fd103029974a2
c00lpoint-t420 <will.c@hotmail.com>
2019-10-10 00:26:46 +0800

    Init projects

---
+++ movie-data-services/src/main/java/com/c00lpoint/test/moviedataservices/MovieDataServicesApplication.java
@@ -0,0 +1,13 @@
+package com.c00lpoint.test.moviedataservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDataServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDataServicesApplication.class, args);
+	}
+
+}
