cf9be9d3f6981e5fb34c6003a240d50282cb93c9
c00lpoint <c00lpoint00@me.com>
2019-10-11 01:47:12 +0800

    add hard code test for movie-data-services

--- movie-data-services/src/main/java/com/c00lpoint/test/moviedataservices/MovieDataController.java
+++ movie-data-services/src/main/java/com/c00lpoint/test/moviedataservices/MovieDataController.java
@@ -5,11 +5,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
+
 @RestController
 @RequestMapping("/movie")
 public class MovieDataController {
+
+    private static final HashMap<Long, String> destMap = new HashMap<Long, String>(){{
+        put(1l, "The Avengers");
+        put(2l, "Age of Ultron");
+        put(3l, " Infinity War");
+        put(4l, "Endgame");
+    }};
+
     @RequestMapping("/{movieId}")
     public MovieInfo getMovieInfo(@PathVariable("movieId") long movieId){
-        return new MovieInfo(movieId, "Avenger 4", "Endgame");
+        return new MovieInfo(movieId, "Avenger " + movieId, destMap.get(movieId));
     }
 }
