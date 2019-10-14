cb3298576f1d2649d7ad1b83842f8ca14ac40bed
chenwei <chenwei@abc.com>
2019-10-14 15:11:13 +0800

    regist restful service as eureka client

--- rating-data-service/src/main/java/com/c00lpoint/test/ratingdataservice/RatingDataServicesApplication.java
+++ rating-data-service/src/main/java/com/c00lpoint/test/ratingdataservice/RatingDataServicesApplication.java
@@ -2,7 +2,9 @@ package com.c00lpoint.test.ratingdataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class RatingDataServicesApplication {
 
