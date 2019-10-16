21549bb29204ed35caedfa8c167c27b584c65e6a
chenwei <chenwei@abc.com>
2019-10-16 15:53:06 +0800

    use load balance call the rsetful service

--- movie-catalog-service/src/main/java/com/c00lpoint/test/moviecatalogservice/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/c00lpoint/test/moviecatalogservice/MovieCatalogController.java
@@ -3,6 +3,7 @@ package com.c00lpoint.test.moviecatalogservice;
 import com.c00lpoint.test.moviecatalogservice.modules.MovieCatalogInfo;
 import com.c00lpoint.test.moviecatalogservice.modules.MovieInfo;
 import com.c00lpoint.test.moviecatalogservice.modules.UserRatings;
+import com.netflix.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,21 +22,23 @@ public class MovieCatalogController {
     @Autowired
     private WebClient.Builder webClientBuilder;
 
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
+
     @RequestMapping("/{userId}")
     public List<MovieCatalogInfo> getMovieCatalog(@PathVariable("userId") String userId){
-
         //get object of list from RestfulService
 //        List<RatingInfo> ratingInfos = template.exchange(
 //                "http://localhost:8083/rating/user/" + userId,
 //                HttpMethod.GET,
 //                null,
 //                new ParameterizedTypeReference<List<RatingInfo>>() {}).getBody();
-        UserRatings userRatings = template.getForObject("http://localhost:8083/rating/user/" + userId, UserRatings.class);
+        UserRatings userRatings = template.getForObject("http://RATING-DATA-SERVICE/rating/user/" + userId, UserRatings.class);
 
         WebClient.Builder builder = WebClient.builder();
 
         return userRatings.getRatings().stream().map(r -> {
-            MovieInfo movieInfo = template.getForObject("http://localhost:8082/movie/" + r.getMovieId(), MovieInfo.class);
+            MovieInfo movieInfo = template.getForObject("http://MOVIE-DATA-SERVICE/movie/" + r.getMovieId(), MovieInfo.class);
 
 //            MovieInfo movieInfo = webClientBuilder.build()
 //                    .get()
